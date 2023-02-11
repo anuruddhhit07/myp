0cfd47bd8f1382437efecbd4b28a8dbc49a408da
Anuruddh Kumar <anuruddh07@gmail.com>
2023-02-11 11:10:11 +0000

    Update barh.jsx

--- src/Component/Svg/barh.jsx
+++ src/Component/Svg/barh.jsx
@@ -6,9 +6,10 @@ class BarChart extends Component {
     super(props);
     this.state = {
       margin: { top: 0, right: 0, bottom: 0, left: 0 },
-      leftmargin:30,
-      barwidth:65,
-      bargap:5
+      leftmargin:150,
+      barwidth:50,
+      bargap:25,
+      bargapy:50
     };
     this.myRef = React.createRef();
   }
@@ -21,6 +22,8 @@ class BarChart extends Component {
     const height = 500 - this.state.margin.top - this.state.margin.bottom;
 
     const data = [120, 50, 60, 70, 90, 100];
+    const datalength=data.length
+    const autobarwidth= (width-2*this.state.leftmargin-this.state.bargap*(datalength-1))/datalength
 
     const svg = d3
       .select(this.myRef.current)
@@ -34,9 +37,9 @@ class BarChart extends Component {
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => this.state.leftmargin + i * (this.state.barwidth+this.state.bargap))
-      .attr("y", (d, i) => 300 - d - 10)
-      .attr("width", this.state.barwidth)
+      .attr("x", (d, i) => this.state.leftmargin + i * (autobarwidth+this.state.bargap))
+      .attr("y", (d, i) => height - d-this.state.bargapy)
+      .attr("width", autobarwidth)
       .attr("height", (d, i) => d)
       .attr("fill", "green");
 
@@ -46,12 +49,12 @@ class BarChart extends Component {
     var xScale = d3
       .scaleBand()
       .domain(data) // This is what is written on the Axis: from 0 to 100
-      .range([this.state.leftmargin, this.state.leftmargin+this.state.barwidth*data.length+this.state.bargap*(data.length-1)]); // This is where the axis is placed: from 100px to 800px
+      .range([this.state.leftmargin, this.state.leftmargin+autobarwidth*datalength+this.state.bargap*(datalength-1)]); // This is where the axis is placed: from 100px to 800px
 
     let xAxisGenerator = d3.axisBottom(xScale).tickSize(-200);
-    let xAxis =  svg.append("g")
+    svg.append("g")
               .call(xAxisGenerator)
-              .attr("transform",`translate(${0},${height - 20})`)
+              .attr("transform",`translate(${0},${height - this.state.bargapy})`)
 
     // Draw the axis
 
