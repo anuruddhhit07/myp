1d07cd10178aaa43e51c74112a114add7ac7c541
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2023-02-16 20:33:21 +0900

    Update ScaterPlot.jsx

--- src/Component/App2/Chart/ScaterPlot.jsx
+++ src/Component/App2/Chart/ScaterPlot.jsx
@@ -1,26 +1,30 @@
 import React, { useRef,useEffect } from "react";
-import ChartD3 from "./ChartD3";
+import AxisD3 from "./Axis3D";
 
-export default function Chart(props) {
-  const { dimensions, msg } = props;
+
+export default function Axis(props) {
+  const { dimensions, data,msg,controller } = props;
   const refElement = useRef(null);
-  // console.log(props);
- 
+//   console.log('Axis',props);
+    
   useEffect(initVis, [ dimensions ]);
 
-
- 
+  
 
   function initVis() {
     if(dimensions) {
       const d3Props = {
         msg,
+        data,
         dimensions,
+        controller
         // onDatapointClick2: function(value){ return setActive(value) }
       };
 
-      let vis = new ChartD3(refElement.current, d3Props);
-      // console.log('vis',vis);
+    //   upseprop=useController1(data,200,300)
+    //   console.log(upseprop);
+      let vis = new AxisD3(refElement.current, d3Props);
+    //   console.log('vis',vis);
     }
   }
 
